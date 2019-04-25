aa6dbae1f45ad88432ba76a64da7d5eb84c61f7c
陆世宝 <18290019296@163.com>
2019-04-25 17:02:35 +0800

    科室医生表操作

---
+++ app/dao/dpDocDao.js
@@ -0,0 +1,44 @@
+var mysqlService = require('./mysqlService');
+
+var dpDocDao = module.exports;
+
+dpDocDao.add = (dd, cb) => {
+  var time = Date.now();
+
+  var sql = 'INSERT INTO oh_department_doctor ' +
+              '(department_id, doctor_id, create_time, update_time) ' + 
+              'VALUES (?, ?, ?, ?)';
+
+  var args = [
+    dd.departmentId,
+    dd.doctorId,
+    time,
+    time
+  ];
+
+  mysqlService.query(sql, args, cb);
+};
+
+dpDocDao.getByDoctorId = (id, cb) => {
+  var sql = 'SELECT * FROM oh_department_doctor WHERE doctor_id = ?';
+
+  var args = [id];
+
+  mysqlService.query(sql, args, cb);
+};
+
+dpDocDao.getByDDId = (departmentId, doctorId, cb) => {
+  var sql = 'SELECT * FROM oh_department_doctor WHERE department_id = ? AND doctor_id = ?';
+
+  var args = [departmentId, doctorId];
+
+  mysqlService.query(sql, args, cb);
+};
+
+dpDocDao.deleteByDoctorId = (id, cb) => {
+  var sql = 'DELETE FROM oh_department_doctor WHERE doctor_id = ?';
+
+  var args = [id];
+
+  mysqlService.delete(sql, args, cb);
+};
