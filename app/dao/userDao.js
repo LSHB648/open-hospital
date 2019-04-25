5255f5ce89d445c2ed67384fa59808f554af8623
陆世宝 <18290019296@163.com>
2019-04-25 14:33:57 +0800

    user表相关操作

---
+++ app/dao/userDao.js
@@ -0,0 +1,76 @@
+var uuid = require('node-uuid');
+var mysqlService = require('./mysqlService');
+
+var userDao = module.exports;
+
+userDao.add = (user, cb) => {
+  var id = uuid.v4();
+  var time = Date.now();
+
+  var sql = 'INSERT INTO oh_user ' +
+              '(id, name, password, type, real_name, ' +
+              'description, card_number, create_time, update_time) ' + 
+              'VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?)';
+
+  var args = [
+    id,
+    user.name,
+    user.password,
+    user.type,
+    user.realName,
+    user.description,
+    user.cardNumber,
+    time,
+    time
+  ];
+
+  mysqlService.query(sql, args, cb);
+};
+
+userDao.getByName = (name, cb) => {
+  var sql = 'SELECT * FROM oh_user WHERE name = ?';
+
+  var args = [name];
+
+  mysqlService.query(sql, args, cb);
+};
+
+userDao.getById = (id, cb) => {
+  var sql = 'SELECT * FROM oh_user WHERE id = ?';
+
+  var args = [id];
+
+  mysqlService.query(sql, args, cb);
+};
+
+userDao.deleteById = (id, cb) => {
+  var sql = 'DELETE FROM oh_user WHERE id = ?';
+
+  var args = [id];
+
+  mysqlService.delete(sql, args, cb);
+};
+
+userDao.updateCardNumber = (user, cb) => {
+  var sql = 'UPDATE oh_user SET card_number = ? WHERE id = ?';
+
+  var args = [user.id, user.cardNumber];
+
+  mysqlService.update(sql, args, cb);
+};
+
+userDao.getAll = (cb) => {
+  var sql = 'SELECT * FROM oh_user';
+
+  var args = [];
+
+  mysqlService.query(sql, args, cb);
+};
+
+userDao.getLogIn = (user, cb) => {
+  var sql = 'SELECT * FROM oh_user WHERE name = ? AND password = ? AND type = ?';
+
+  var args = [user.name, user.password, user.type];
+
+  mysqlService.query(sql, args, cb);
+};
