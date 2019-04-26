119e5f525afe083835b211141fc5cf521ada4a0d
陆世宝 <18290019296@163.com>
2019-04-26 14:54:55 +0800

    医生用户平台支持的操作

--- app/service/doctorHandler.js
+++ app/service/doctorHandler.js
@@ -5,5 +5,62 @@ var doctorHandler = module.exports;
 var handler = {};
 
 doctorHandler.getHandler = () => {
+  handler[constx.ACTION.getUser] = getUser;
+  handler[constx.ACTION.logIn] = logIn;
+  handler[constx.ACTION.logOut] = logOut;
+  handler[constx.ACTION.getDepartment] = getDepartment;
+  handler[constx.ACTION.listDepartment] = listDepartment;
+  handler[constx.ACTION.listSchedule] = listSchedule;
+  handler[constx.ACTION.getGuide] = getGuide;
+  handler[constx.ACTION.listRegistration] = listRegistration;
+  handler[constx.ACTION.callRegistration] = callRegistration;
+  handler[constx.ACTION.addPrescription] = addPrescription;
+  handler[constx.ACTION.listPrescription] = listPrescription;
+  handler[constx.ACTION.addCharge] = addCharge;
+
   return handler;
 };
+
+function getUser(req, cb) {
+  return;
+}
+
+function logIn(req, cb) {
+  return;
+}
+
+function logOut(req, cb) {
+  return;
+}
+
+function getDepartment(req, cb) {
+  return;
+}
+
+function listSchedule(req, cb) {
+  return;
+}
+
+function getGuide(req, cb) {
+  return;
+}
+
+function listRegistration(req, cb) {
+  return;
+}
+
+function callRegistration(req, cb) {
+  return;
+}
+
+function addPrescription(req, cb) {
+  return;
+}
+
+function listPrescription(req, cb) {
+  return;
+}
+
+function addCharge(req, cb) {
+  return;
+}
