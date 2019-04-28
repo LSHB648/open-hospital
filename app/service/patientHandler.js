5ddd088964f3362444b6b1dcaf27d87e5001328b
陆世宝 <18290019296@163.com>
2019-04-28 20:13:20 +0800

    门诊用户平台查询所有处方操作

--- app/service/patientHandler.js
+++ app/service/patientHandler.js
@@ -7,6 +7,7 @@ var cookieService = require('./cookieService');
 var userDao = require('../dao/userDao');
 var scheduleDao = require('../dao/scheduleDao');
 var registrationDao = require('../dao/registrationDao');
+var prescriptionDao = require('../dao/prescriptionDao');
 var adminHandler = require('./adminHandler');
 var logger = require('../log/logger').getLogger('main');
 
@@ -428,7 +429,50 @@ function listRegistration(req) {
 }
 
 function listPrescription(req) {
-  return;
+  async.waterfall([
+    (func) => {
+      var ckDec = cookieService.decode(req.msg.Cookie);
+      var key = constx.PREFIX.cookieCache + ckDec.userId;
+
+      req.msg.UserId = ckDec.userId;
+      redisService.getKey(key, func);
+
+    }, (cookie, func) => {
+      if (cookie !== req.msg.Cookie) {
+        logger.error("req para Cookie wrong or expired");
+        req.paraName = 'Cookie';
+        req.paraVal = req.msg.Cookie;
+        return req.conn.sendText(response.getStr(req, 404));
+      }
+
+      prescriptionDao.getByUserId(req.msg.UserId, func);
+    }
+  ], (err, res) => {
+    if (!!err) {
+      logger.error("listPrescription internal error = %s", err);
+      return req.conn.sendText(response.getStr(req, 407));
+    }
+
+    logger.info("listPrescription success");
+
+    var pres = [];
+
+    for (var p of res) {
+      var pre = {};
+
+      pre.Id = p.id;
+      pre.UserId = p.user_id;
+      pre.DepartmentId = p.department_id;
+      pre.DoctorId = p.doctor_id;
+      pre.Content = p.content;
+
+      pres.push(pre);
+    }
+
+    var ret = response.getJson(req, 200);
+    ret.Prescriptions = pres;
+    return req.conn.sendText(JSON.stringify(ret));
+  });
 }
 
 function onlinePay(req) {
