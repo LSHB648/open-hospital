a4341c72e8bb542859280491a976390b5f1593a9
陆世宝 <18290019296@163.com>
2019-04-29 11:24:35 +0800

    简易测试客户端修正

--- test/client.js
+++ test/client.js
@@ -8,8 +8,8 @@ req.PassWord = Buffer.from('iamhzlushiliang').toString('base64');
 req.Type = 'Patient';
 req.RealName = '陆世亮';
 
-ws.connect("ws://127.0.0.1:8080", (conn) => {
-  console.info("websocket client connected to server");
+var conn = ws.connect("ws://127.0.0.1:8080", (err) => {
+  console.info("websocket client connected to server, err = %j", err);
 
   conn.sendText(JSON.stringify(req));
 
