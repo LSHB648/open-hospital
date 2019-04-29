9528961bc43d86e0e2503b74d153f1c96d555616
陆世宝 <18290019296@163.com>
2019-04-29 17:49:57 +0800

    key在mysql中是一个保留字

--- app/dao/guideDao.js
+++ app/dao/guideDao.js
@@ -6,7 +6,7 @@ guideDao.add = (gd, cb) => {
   var time = Date.now();
 
   var sql = 'INSERT INTO oh_guide ' +
-              '(key, value, create_time, update_time) ' + 
+              '(`key`, value, create_time, update_time) ' + 
               'VALUES (?, ?, ?, ?)';
 
   var args = [
@@ -20,7 +20,7 @@ guideDao.add = (gd, cb) => {
 };
 
 guideDao.getByKey = (key, cb) => {
-  var sql = 'SELECT * FROM oh_guide WHERE key = ?';
+  var sql = 'SELECT * FROM oh_guide WHERE `key` = ?';
 
   var args = [key];
 
@@ -44,7 +44,7 @@ guideDao.getAll = (cb) => {
 guideDao.updateValue = (gd, cb) => {
   var time = Date.now();
 
-  var sql = 'UPDATE oh_guide SET value = ? , update_time = ? WHERE key = ?';
+  var sql = 'UPDATE oh_guide SET value = ? , update_time = ? WHERE `key` = ?';
 
   var args = [gd.value, time, gd.key];
 
